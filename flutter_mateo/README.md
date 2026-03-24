cfbb00cb13d88a4ba2400913617b6df3238fdb54
mare536 <mre536@vidalibarraquer.net>
2026-03-24 10:43:07 +0100

    primer

---
+++ flutter_mateo/README.md
@@ -0,0 +1,3 @@
+# flutter_mateo
+
+A new Flutter project.
---
+++ flutter_mateo/android/app/src/main/kotlin/com/example/flutter_mateo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_mateo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
